ce92074ed080c40246bfed4db2c73a5d9e3192ef
rascal <nalpa199858@gmail.com>
2023-12-11 13:21:14 -0800

    porttfolio

---
+++ src/js/UI/About/skills.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        name: 'Laravel',
+        width: '100%',
+    },
+    {
+        name: 'React/NextJS',
+        width: '100%',
+    },
+    {
+        name: 'Vue/NuxtJs',
+        width: '100%',
+    },
+    {
+        name: 'Angular',
+        width: '90%'
+    },
+    {
+        name: 'Django',
+        width: '90%',
+    },
+    {
+        name: 'Wordpress',
+        width: '90%',
+    },
+    {
+        name: 'Symfony',
+        width: '90%',
+    },
+]
